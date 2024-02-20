5b83efe40c2a75a20b0994f33ccffc342da42a4a
Lucas Dantas <lucasdantasprogramador@gmail.com>
2024-02-20 13:17:03 -0300

    first commit

---
+++ wp_eventos_mirante.php
@@ -0,0 +1,15 @@
+<?php
+/**
+* Plugin Name: Eventos do Mirante
+* Plugin URI: https://github.com/lucassdantas/wp_eventos_mirante.git
+* Description: Eventos do Mirante do Arvrão
+* Version: 0.1
+* Author: R&D Marketing Digital 
+* Author URI: https://rdmarketing.com.br/
+**/
+
+defined('ABSPATH') or die();
+if(!function_exists('add_action')){
+    die;
+}
+
